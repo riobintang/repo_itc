969ede0958f3bd343c57e62988e4ffa0cc26fe34
Anindia Azzahra <102598538+anindiaazzahra@users.noreply.github.com>
2022-10-31 13:56:03 +0700

    fixed error cors origin

--- app.js
+++ app.js
@@ -4,9 +4,17 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const userRouter = require("./app/user/route");
 const divisionRouter = require("./app/division/route");
+const courseRouter = require("./app/course/route");
 const customErrorHandler = require("./middleware/customErrorHandler");
 const handler404NotFound = require("./middleware/handler404NotFound");
 const app = express();
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,6 +24,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/user", userRouter);
 app.use("/division", divisionRouter);
+app.use("/course", courseRouter);
+
 
 app.use(customErrorHandler);
 app.use(handler404NotFound);
