ecba8a08afa4de65b62e8e03aab99dedd094a8f4
RoxasDavega <bintang.magician@gmail.com>
2022-11-02 22:25:32 +0700

    add comment and edit sendEmail

--- app/user/route.js
+++ app/user/route.js
@@ -2,11 +2,11 @@ const express = require("express");
 const { handlerRegisterUser, handlerUserLogin, handlerGetUserById } = require("./handler");
 const router = express.Router();
 
-//API Register user: user/register
+//API Register user: POST user/register
 router.post("/register", handlerRegisterUser);
-//API User Login: user/login
+//API User Login: POST user/login
 router.post("/login", handlerUserLogin);
-//API Get User by id: user/:id
+//API Get User by id: GET user/:id
 router.get("/:id", handlerGetUserById);
 
 module.exports = router;
