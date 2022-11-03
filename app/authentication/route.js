a809cfd4d52e90de745c4339c1769c1193b51865
RoxasDavega <bintang.magician@gmail.com>
2022-11-03 12:40:41 +0700

    update refresh token

--- app/authentication/route.js
+++ app/authentication/route.js
@@ -1,9 +1,9 @@
-const { application } = require('express');
 const express = require('express');
+const authenticationToken = require('../../middleware/authenticationToken');
 const { refreshJWTHandler } = require('./handler');
 const router = express.Router();
 
 //API post refresh token: POST /token/refresh
-router.post('/refresh', refreshJWTHandler);
+router.post('/refresh', authenticationToken, refreshJWTHandler);
 
 module.exports = router;
