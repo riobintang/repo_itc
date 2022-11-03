66f9bf3580dca204f401d7a83ee4993d85cace9d
RoxasDavega <bintang.magician@gmail.com>
2022-11-03 10:08:05 +0700

    try fix

--- middleware/customErrorHandler/index.js
+++ middleware/customErrorHandler/index.js
@@ -3,6 +3,7 @@ function customErrorHandler(error, req, res, next) {
   res.status(400).json({
     status: "error",
     message: error.message,
+    data: { error: error.message, stack: error.stack },
   });
 }
 
