5e43ed8960e6bb8e89cb50c6ddfb3801f626aff2
Anindia Azzahra <102598538+anindiaazzahra@users.noreply.github.com>
2022-11-06 18:49:15 +0700

    fixed CORS error

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ require('dotenv').config();
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
