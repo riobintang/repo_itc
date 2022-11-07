c02520a69923432fc820428d90d9346eac346c2c
Anindia Azzahra <102598538+anindiaazzahra@users.noreply.github.com>
2022-11-07 09:43:12 +0700

    Delete app/authentication directory

--- app/authentication/route.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const authenticationToken = require('../../middleware/authenticationToken');
-const { refreshJWTHandler } = require('./handler');
-const router = express.Router();
-
-//API post refresh token: POST /token/refresh
-router.post('/refresh', authenticationToken, refreshJWTHandler);
-
-module.exports = router;
