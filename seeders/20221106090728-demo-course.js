26384a4cce1179539e6f3d976e2d2760738dfdba
RoxasDavega <bintang.magician@gmail.com>
2023-01-16 09:59:29 +0700

    fixed seeder course

--- seeders/20221106090728-demo-course.js
+++ seeders/20221106090728-demo-course.js
@@ -1,31 +1,105 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert("courses", [
       {
-        id: 1,
+        
         title: "Javascript Dasar",
         description:
           "Course ini akan mengajarkan kalian dasar-dasar javascript",
         image_thumbnail:
           "https://res.cloudinary.com/dd6stok7k/image/upload/v1667643077/itc-repo/course/qvb6qhhlvyo1x69fox16.jpg",
         cloudinary_id: "qvb6qhhlvyo1x69fox16",
-        id_division: 2, 
+        id_division: 2,
         id_user: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
       },
       {
-        id: 2,
+        
         title: "Python Dasar",
-        description:
-          "Course ini akan mengajarkan kalian dasar-dasar python",
+        description: "Course ini akan mengajarkan kalian dasar-dasar python",
         image_thumbnail:
           "https://res.cloudinary.com/dd6stok7k/image/upload/v1667634116/itc-repo/course/b2zkflzfmac6bjiq9aqd.jpg",
         cloudinary_id: "b2zkflzfmac6bjiq9aqd",
-        id_division: 2, 
+        id_division: 2,
+        id_user: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        
+        title: "Dart Dasar",
+        description:
+          "Course ini akan mengajarkan kalian dasar-dasar javascript",
+        image_thumbnail:
+          "https://res.cloudinary.com/dd6stok7k/image/upload/v1667643077/itc-repo/course/qvb6qhhlvyo1x69fox16.jpg",
+        cloudinary_id: "qvb6qhhlvyo1x69fox16",
+        id_division: 3,
+        id_user: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        
+        title: "Figma From Scratch",
+        description: "Course ini akan mengajarkan kalian dasar-dasar python",
+        image_thumbnail:
+          "https://res.cloudinary.com/dd6stok7k/image/upload/v1667634116/itc-repo/course/b2zkflzfmac6bjiq9aqd.jpg",
+        cloudinary_id: "b2zkflzfmac6bjiq9aqd",
+        id_division: 4,
+        id_user: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        
+        title: "SQL Dasar",
+        description:
+          "Course ini akan mengajarkan kalian dasar-dasar javascript",
+        image_thumbnail:
+          "https://res.cloudinary.com/dd6stok7k/image/upload/v1667643077/itc-repo/course/qvb6qhhlvyo1x69fox16.jpg",
+        cloudinary_id: "qvb6qhhlvyo1x69fox16",
+        id_division: 1,
+        id_user: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        
+        title: "Pengenalan Flutter",
+        description: "Course ini akan mengajarkan kalian dasar-dasar python",
+        image_thumbnail:
+          "https://res.cloudinary.com/dd6stok7k/image/upload/v1667634116/itc-repo/course/b2zkflzfmac6bjiq9aqd.jpg",
+        cloudinary_id: "b2zkflzfmac6bjiq9aqd",
+        id_division: 3,
+        id_user: 2,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        
+        title: "Pengenalan UX",
+        description:
+          "Course ini akan mengajarkan kalian dasar-dasar javascript",
+        image_thumbnail:
+          "https://res.cloudinary.com/dd6stok7k/image/upload/v1667643077/itc-repo/course/qvb6qhhlvyo1x69fox16.jpg",
+        cloudinary_id: "qvb6qhhlvyo1x69fox16",
+        id_division: 5,
+        id_user: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        
+        title: "React JS for Front End",
+        description: "Course ini akan mengajarkan kalian dasar-dasar python",
+        image_thumbnail:
+          "https://res.cloudinary.com/dd6stok7k/image/upload/v1667634116/itc-repo/course/b2zkflzfmac6bjiq9aqd.jpg",
+        cloudinary_id: "b2zkflzfmac6bjiq9aqd",
+        id_division: 2,
         id_user: 1,
         createdAt: new Date(),
         updatedAt: new Date(),
@@ -33,7 +107,7 @@ module.exports = {
     ]);
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete("courses", null, [{}]);
-  }
+  },
 };
