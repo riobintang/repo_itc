3b2cf62b5e25c8c4cf0c760c7a620465a37946fa
RoxasDavega <bintang.magician@gmail.com>
2023-02-06 19:47:51 +0700

    change method

--- app/user/route.js
+++ app/user/route.js
@@ -15,10 +15,10 @@ router.get("/:id", authenticationToken, handlerGetUserById);
 router.post("/refresh-token", refreshJWTHandler);
 // API Update User Profile: PUT user/update/:id
 router.put("/update/:id", authenticationToken, upload.single("image"), handlerPutUserProfile);
-// API User Change Password: PUT user/changepassword/:id
-router.put("/changepassword/:id", authenticationToken, handlerChangePassword);
+// API User Change Password: POST user/changepassword/:id
+router.post("/changepassword/:id", authenticationToken, handlerChangePassword);
 // API Update User Profile and Password: PUT user/updateprofilepassword/:id
 router.put("/updateprofilepassword/:id", authenticationToken, upload.single("image"), handlerPutUserProfilePassword);
-
+// API Verify User: Put user/verify/:id
 router.put("/verify/:id", authenticationToken, loginAdmin, handlerPutVerifyUser);
 module.exports = router;
